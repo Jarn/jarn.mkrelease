0137c6192191ab6d102d6702b7e040bf1e91058c
Stefan H. Holek <stefan@jarn.com>
2009-01-26 15:17:04 +0000

    Post-release version bump.

--- jarn/mkrelease/mkrelease.py
+++ jarn/mkrelease/mkrelease.py
@@ -11,7 +11,7 @@ python = "python2.4"
 distbase = "jarn.com:/home/psol/dist"
 distdefault = "public"
 
-version = "mkrelease 0.18"
+version = "mkrelease 0.19"
 usage = """\
 Usage: mkrelease [-CTSDK] [-cz] [-d dist-location] [svn-url|svn-sandbox]
        mkrelease [-CTSDK] [-cz] [-p [-s [-i identity]]] [svn-url|svn-sandbox]
