1b6fb727634dbcb85af69ae01b813769eead56ce
Stefan H. Holek <stefan@jarn.com>
2009-06-26 17:09:12 +0000

    Use full name in version string.

--- jarn/mkrelease/mkrelease.py
+++ jarn/mkrelease/mkrelease.py
@@ -18,7 +18,7 @@ distdefault = ""
 pypiurl = "http://pypi.python.org/pypi"
 maxaliasdepth = 23
 
-version = "mkrelease 2.0a2"
+version = "jarn.mkrelease 2.0a2"
 usage = "Try 'mkrelease --help' for more information."
 help = """\
 Usage: mkrelease [options] [scm-url|scm-sandbox]
