e04417e1ca0fd786a7d7cfc5b8d970d083277aaa
Stefan H. Holek <stefan@epy.co.at>
2011-10-31 10:58:47 +0100

    Make Python class testable and add tests.

--- jarn/mkrelease/python.py
+++ jarn/mkrelease/python.py
@@ -6,16 +6,20 @@ from exit import err_exit
 class Python(object):
     """Python interpreter abstraction."""
 
-    @property
-    def python(self):
-        return sys.executable
+    def __init__(self, python=None, version_info=None):
+        self.python = sys.executable
+        self.version_info = sys.version_info
+        if python is not None:
+            self.python = python
+        if version_info is not None:
+            self.version_info = version_info
 
     def __str__(self):
         return self.python
 
     def is_valid_python(self):
-        return (sys.version_info[:2] >= (2, 6) and
-                sys.version_info[:2] < (3, 0))
+        return (self.version_info[:2] >= (2, 6) and
+                self.version_info[:2] < (3, 0))
 
     def check_valid_python(self):
         if not self.is_valid_python():
---
+++ jarn/mkrelease/tests/test_python.py
@@ -0,0 +1,49 @@
+import unittest
+import sys
+
+from jarn.mkrelease.python import Python
+
+from jarn.mkrelease.testing import quiet
+
+
+class PythonTests(unittest.TestCase):
+
+    def testDefaults(self):
+        python = Python()
+        self.assertEqual(python.python, sys.executable)
+        self.assertEqual(python.version_info, sys.version_info)
+
+    def testArguments(self):
+        python = Python(python='/foo/python', version_info=(2, 7, 0))
+        self.assertEqual(python.python, '/foo/python')
+        self.assertEqual(python.version_info, (2, 7, 0))
+
+    def testStrCoerce(self):
+        python = Python(python='/foo/python')
+        self.assertEqual(str(python), '/foo/python')
+
+    def test26IsValidPython(self):
+        python = Python(version_info=(2, 6, 0))
+        self.assertEqual(python.is_valid_python(), True)
+
+    def test27IsValidPython(self):
+        python = Python(version_info=(2, 7, 0))
+        self.assertEqual(python.is_valid_python(), True)
+
+    def test25IsInvalidPython(self):
+        python = Python(version_info=(2, 5, 0))
+        self.assertEqual(python.is_valid_python(), False)
+
+    def test30IsInvalidPython(self):
+        python = Python(version_info=(3, 0, 0))
+        self.assertEqual(python.is_valid_python(), False)
+
+    @quiet
+    def testCheckValidPythonRaises(self):
+        python = Python(version_info=(3, 0, 0))
+        self.assertRaises(SystemExit, python.check_valid_python)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
+
