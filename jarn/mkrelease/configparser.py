09697b4571175960d82491a6942e96537204b6ef
Stefan H. Holek <stefan@epy.co.at>
2011-11-09 18:22:42 +0100

    Improve ConfigParser error messages.

--- jarn/mkrelease/configparser.py
+++ jarn/mkrelease/configparser.py
@@ -15,7 +15,7 @@ class ConfigParser(SafeConfigParser, object):
             try:
                 return super(ConfigParser, self).getboolean(section, option)
             except ValueError, e:
-                warn(e)
+                warn('Not a boolean: %s = %s' % (option, self._value_from_exc(e)))
         return default
 
     def getint(self, section, option, default=None):
@@ -23,7 +23,7 @@ class ConfigParser(SafeConfigParser, object):
             try:
                 return super(ConfigParser, self).getint(section, option)
             except ValueError, e:
-                warn(e)
+                warn('Not an integer: %s = %s' % (option, self._value_from_exc(e)))
         return default
 
     def getfloat(self, section, option, default=None):
@@ -31,6 +31,18 @@ class ConfigParser(SafeConfigParser, object):
             try:
                 return super(ConfigParser, self).getfloat(section, option)
             except ValueError, e:
-                warn(e)
+                warn('Not a float: %s = %s' % (option, self._value_from_exc(e)))
         return default
+
+    def _value_from_exc(self, exc):
+        # e.g.: invalid literal for int() with base 10: 'foo'
+        msg = str(exc)
+        colon = msg.find(':')
+        if colon >= 0:
+            value = msg[colon+1:].strip()
+            if (value.startswith("'") and value.endswith("'")) or \
+               (value.startswith('"') and value.endswith('"')):
+                value = value[1:-1]
+            return value
+        return ''
 
