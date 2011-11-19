8c292ea6b6ba3d8a48a524a0e6d7da9d98b71cdf
Stefan H. Holek <stefan@epy.co.at>
2011-11-19 11:02:59 +0100

    Warn when encountering multiple values in non-string options.

--- jarn/mkrelease/configparser.py
+++ jarn/mkrelease/configparser.py
@@ -1,8 +1,19 @@
 from ConfigParser import SafeConfigParser
+from ConfigParser import Error
 from exit import warn
 
 
+class MultipleValueError(Error):
+    pass
+
+
 class ConfigParser(SafeConfigParser, object):
+
+    def read(self, filenames):
+        try:
+            super(ConfigParser, self).read(filenames)
+        except Error, e:
+            warn(e)
 
     def get(self, section, option, default=None):
         if self.has_option(section, option):
@@ -11,34 +22,61 @@ class ConfigParser(SafeConfigParser, object):
 
     def getboolean(self, section, option, default=None):
         if self.has_option(section, option):
+            value = super(ConfigParser, self).get(section, option)
             try:
-                return super(ConfigParser, self).getboolean(section, option)
+                return self._to_boolean(value)
+            except MultipleValueError, e:
+                warn("Multiple values not allowed: %s = %r" % (option, self._value_from_exc(e)))
             except ValueError, e:
-                warn('Not a boolean: %s = %s' % (option, self._value_from_exc(e)))
+                warn('Not a boolean: %s = %r' % (option, self._value_from_exc(e)))
         return default
 
     def getint(self, section, option, default=None):
         if self.has_option(section, option):
+            value = super(ConfigParser, self).get(section, option)
             try:
-                return super(ConfigParser, self).getint(section, option)
+                return self._to_int(value)
+            except MultipleValueError, e:
+                warn('Multiple values not allowed: %s = %r' % (option, self._value_from_exc(e)))
             except ValueError, e:
-                warn('Not an integer: %s = %s' % (option, self._value_from_exc(e)))
+                warn('Not an integer: %s = %r' % (option, self._value_from_exc(e)))
         return default
 
     def getfloat(self, section, option, default=None):
         if self.has_option(section, option):
+            value = super(ConfigParser, self).get(section, option)
             try:
-                return super(ConfigParser, self).getfloat(section, option)
+                return self._to_float(value)
+            except MultipleValueError, e:
+                warn('Multiple values not allowed: %s = %r' % (option, self._value_from_exc(e)))
             except ValueError, e:
-                warn('Not a float: %s = %s' % (option, self._value_from_exc(e)))
+                warn('Not a float: %s = %r' % (option, self._value_from_exc(e)))
         return default
+
+    def _convertable(self, value):
+        v = value.lower().strip()
+        if len(v.split()) > 1:
+            raise MultipleValueError('Multiple values not allowed: %s' % value)
+        return v
+
+    def _to_boolean(self, value):
+        v = self._convertable(value)
+        if v not in self._boolean_states:
+            raise ValueError('Not a boolean: %s' % value)
+        return self._boolean_states[v]
+
+    def _to_int(self, value):
+        return int(self._convertable(value))
+
+    def _to_float(self, value):
+        return float(self._convertable(value))
 
     def _value_from_exc(self, exc):
         # e.g.: invalid literal for int() with base 10: 'a'
         msg = str(exc)
         colon = msg.find(':')
         if colon >= 0:
-            value = msg[colon+1:].strip()
+            value = msg[colon+1:].lstrip(' \t')
             if (value.startswith("'") and value.endswith("'")) or \
                (value.startswith('"') and value.endswith('"')):
                 value = value[1:-1]
