6ee44ec00461850966adad7ecf0e15e599a33ec7
Stefan H. Holek <stefan@epy.co.at>
2012-07-26 00:21:41 +0200

    Remove unused utility functions.

--- jarn/mkrelease/utils.py
+++ jarn/mkrelease/utils.py
@@ -15,22 +15,3 @@ def decode(string):
 def encode(string):
     """Encode to the charset of the current locale."""
     return string.encode(locale.getlocale()[1], errors)
-
-
-def char(int):
-    """Create a one-character byte string from the ordinal ``int``."""
-    if sys.version_info[0] >= 3:
-        return bytes((int,))
-    else:
-        return chr(int)
-
-
-def b(string, encoding='utf-8'):
-    """Used instead of b'' literals to stay Python 2.5 compatible.
-
-    ``encoding`` should match the encoding of the source file.
-    """
-    if isinstance(string, unicode):
-        return string.encode(encoding)
-    return string
-
