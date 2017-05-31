d8d7afcd012932f14a8a613fe56b96eddd3f5719
Stefan H. Holek <stefan@epy.co.at>
2017-05-31 20:20:21 +0200

    Support multiple archive formats per release.

--- jarn/mkrelease/mkrelease.py
+++ jarn/mkrelease/mkrelease.py
@@ -243,9 +243,8 @@ class ReleaseMaker(object):
         self.identity = ''
         self.branch = ''
         self.scmtype = ''
-        self.distcmd = 'sdist'
         self.infoflags = self.setuptools.infoflags
-        self.distflags = ['--formats="zip"']
+        self.distributions = []
         self.directory = os.curdir
         self.scm = None
 
@@ -306,17 +305,13 @@ class ReleaseMaker(object):
                 self.skiptag = True
                 self.infoflags = []
             elif name in ('-z', '--zip'):
-                self.distcmd = 'sdist'
-                self.distflags = ['--formats="zip"']
+                self.distributions.append(('sdist', ['--formats="zip"']))
             elif name in ('-g', '--gztar'):
-                self.distcmd = 'sdist'
-                self.distflags = ['--formats="gztar"']
+                self.distributions.append(('sdist', ['--formats="gztar"']))
             elif name in ('-b', '--binary'):
-                self.distcmd = 'bdist'
-                self.distflags = ['--formats="egg"']
+                self.distributions.append(('bdist', ['--formats="egg"']))
             elif name in ('-w', '--wheel'):
-                self.distcmd = 'bdist_wheel'
-                self.distflags = []
+                self.distributions.append(('bdist_wheel', []))
             elif name in ('-c', '--config-file') and depth == 0:
                 self.reset_defaults(abspath(expanduser(value)))
                 return self.parse_options(args, depth+1)
@@ -386,6 +381,9 @@ class ReleaseMaker(object):
         if args:
             self.directory = args[0]
 
+        if not self.distributions:
+            self.distributions.append(('sdist', ['--formats="zip"']))
+
         if self.list:
             self.list_locations()
 
@@ -439,8 +437,6 @@ class ReleaseMaker(object):
         """
         directory = self.directory
         infoflags = self.infoflags
-        distcmd = self.distcmd
-        distflags = self.distflags
         branch = self.branch
         scmtype = self.scm.name
         develop = not self.infoflags
@@ -479,30 +475,31 @@ class ReleaseMaker(object):
                 self.scm.check_tag_exists(directory, tagid)
                 self.scm.create_tag(directory, tagid, name, version, self.push)
 
-            manifest = self.setuptools.run_egg_info(
-                directory, infoflags, scmtype, self.quiet)
-            distfile = self.setuptools.run_dist(
-                directory, infoflags, distcmd, distflags, scmtype, self.quiet)
+            for distcmd, distflags in self.distributions:
+                manifest = self.setuptools.run_egg_info(
+                    directory, infoflags, scmtype, self.quiet)
+                distfile = self.setuptools.run_dist(
+                    directory, infoflags, distcmd, distflags, scmtype, self.quiet)
 
-            for location in self.locations:
-                if self.locations.is_server(location):
-                    if not self.get_skipregister(location):
-                        self.setuptools.run_register(
-                            directory, infoflags, location, scmtype, self.quiet)
-                    if not self.skipupload:
-                        uploadflags = self.get_uploadflags(location)
-                        if '--sign' in uploadflags and isfile(distfile+'.asc'):
-                            os.remove(distfile+'.asc')
-                        self.setuptools.run_upload(
-                            directory, infoflags, distcmd, distflags, location, uploadflags,
-                            scmtype, self.quiet)
-                else:
-                    if not self.skipupload:
-                        if self.locations.is_dist_url(location):
-                            scheme, location = self.urlparser.to_ssh_url(location)
-                            self.scp.run_upload(scheme, distfile, location)
-                        else:
-                            self.scp.run_upload('scp', distfile, location)
+                for location in self.locations:
+                    if self.locations.is_server(location):
+                        if not self.get_skipregister(location):
+                            self.setuptools.run_register(
+                                directory, infoflags, location, scmtype, self.quiet)
+                        if not self.skipupload:
+                            uploadflags = self.get_uploadflags(location)
+                            if '--sign' in uploadflags and isfile(distfile+'.asc'):
+                                os.remove(distfile+'.asc')
+                            self.setuptools.run_upload(
+                                directory, infoflags, distcmd, distflags, location, uploadflags,
+                                scmtype, self.quiet)
+                    else:
+                        if not self.skipupload:
+                            if self.locations.is_dist_url(location):
+                                scheme, location = self.urlparser.to_ssh_url(location)
+                                self.scp.run_upload(scheme, distfile, location)
+                            else:
+                                self.scp.run_upload('scp', distfile, location)
         finally:
             shutil.rmtree(tempdir)
 
