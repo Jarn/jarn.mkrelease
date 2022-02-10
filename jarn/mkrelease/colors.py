0f43cdbc78a66d86877455f92e890a949f075f2e
Stefan H. Holek <stefan@epy.co.at>
2022-02-10 14:14:19 +0100

    Depend on blessed for colors.

--- jarn/mkrelease/colors.py
+++ jarn/mkrelease/colors.py
@@ -1,26 +1,10 @@
-import sys
+import blessed
 
 
-def plain(string):
-    return '\033[0m'+string if sys.stdout.isatty() else string
+term = blessed.Terminal()
 
-
-def bold(string):
-    return '\033[0;1m'+string+'\033[0m' if sys.stdout.isatty() else string
-
-
-def red(string):
-    return '\033[0;31;1m'+string+'\033[0m' if sys.stdout.isatty() else string
-
-
-def green(string):
-    return '\033[0;32;1m'+string+'\033[0m' if sys.stdout.isatty() else string
-
-
-def yellow(string):
-    return '\033[0;33;1m'+string+'\033[0m' if sys.stdout.isatty() else string
-
-
-def blue(string):
-    return '\033[0;34;1m'+string+'\033[0m' if sys.stdout.isatty() else string
+bold = term.bold
+blue = term.bold_blue
+green = term.bold_green
+red = term.bold_red
 
