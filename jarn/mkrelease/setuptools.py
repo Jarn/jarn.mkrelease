c798f1cfc506ca033d3b21e99708d99161371244
Stefan H. Holek <stefan@epy.co.at>
2023-09-19 11:51:42 +0200

    Filter deprecated config warning.

--- jarn/mkrelease/setuptools.py
+++ jarn/mkrelease/setuptools.py
@@ -26,7 +26,8 @@ GONE_RESPONSE = 'Server response (410):'
 FILTERWARNINGS = ('-W "ignore:setup.py install is deprecated" '
                   '-W "ignore:easy_install command is deprecated" '
                   '-W "ignore:Support for \`[tool.setuptools]\` in \`pyproject.toml\`" '
-                  '-W "ignore:The namespace_packages parameter is deprecated"')
+                  '-W "ignore:The namespace_packages parameter is deprecated" '
+                  '-W "ignore:Deprecated config in \`setup.cfg\`"')
 
 
 class Setuptools(object):
